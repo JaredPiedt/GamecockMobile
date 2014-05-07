28bbb376b636f0e4ef1e53e6d5570306ec48158b
JaredPiedt <jared.piedt@gmail.com>
2014-05-06 22:29:41 -0400

    functionality for date picker in AddEventActivity

--- GamecockMobile/src/com/gamecockmobile/events/AddEventActivity.java
+++ GamecockMobile/src/com/gamecockmobile/events/AddEventActivity.java
@@ -1,6 +1,7 @@
 package com.gamecockmobile.events;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 
 import com.gamecockmobile.Course;
 import com.gamecockmobile.DatabaseHandler;
@@ -9,10 +10,17 @@ import com.gamecockmobile.R.layout;
 import com.gamecockmobile.R.menu;
 
 import android.os.Bundle;
+import android.annotation.SuppressLint;
 import android.app.ActionBar;
 import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+
 import android.content.Context;
 import android.content.Intent;
+import android.text.format.DateUtils;
+import android.text.format.Time;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -21,13 +29,17 @@ import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.Spinner;
+import android.widget.TimePicker;
 
 public class AddEventActivity extends Activity implements OnClickListener {
 
   Spinner mSelectCourseSpinner;
   Button mDateButton;
   Spinner mRemindersSpinner;
+
+  private String mTimeZone;
 
   DatabaseHandler db;
   private ArrayList<Course> mCourses = new ArrayList<Course>();
@@ -70,6 +82,7 @@ public class AddEventActivity extends Activity implements OnClickListener {
     db = new DatabaseHandler(this);
 
     mCourses = db.getAllCourses();
+    mTimeZone = Time.getCurrentTimezone();
 
     for (int i = 0; i < mCourses.size(); i++) {
       mCourseNames.add(mCourses.get(i).getCourseName());
@@ -77,6 +90,7 @@ public class AddEventActivity extends Activity implements OnClickListener {
     }
 
     mSelectCourseSpinner = (Spinner) findViewById(R.id.selectCourse_spinner);
+    mDateButton = (Button) findViewById(R.id.date_button);
     mRemindersSpinner = (Spinner) findViewById(R.id.reminders_spinner);
 
     // Create an ArrayAdapter using the string array and a default spinner layout
@@ -84,11 +98,11 @@ public class AddEventActivity extends Activity implements OnClickListener {
         android.R.layout.simple_spinner_item, mCourseNames);
     selectCourseAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
     mSelectCourseSpinner.setAdapter(selectCourseAdapter);
-    
- // Create an ArrayAdapter using the string array and a default spinner layout
+
+    // Create an ArrayAdapter using the string array and a default spinner layout
     ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-            R.array.reminders_array, android.R.layout.simple_spinner_item);
- // Specify the layout to use when the list of choices appears
+        R.array.reminders_array, android.R.layout.simple_spinner_item);
+    // Specify the layout to use when the list of choices appears
     adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
     // Apply the adapter to the spinner
     mRemindersSpinner.setAdapter(adapter);
@@ -111,4 +125,45 @@ public class AddEventActivity extends Activity implements OnClickListener {
     }
   }
 
+  @SuppressLint("ValidFragment")
+  private class DatePickerFragment extends DialogFragment implements
+      DatePickerDialog.OnDateSetListener {
+
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+      // Use the current date as the default date in the picker
+      final Calendar c = Calendar.getInstance();
+      int year = c.get(Calendar.YEAR);
+      int month = c.get(Calendar.MONTH);
+      int day = c.get(Calendar.DAY_OF_MONTH);
+
+      // Create a new instance of DatePickerDialog and return it
+      return new DatePickerDialog(getActivity(), this, year, month, day);
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int day) {
+      // TODO Auto-generated method stub
+      Time date = new Time(mTimeZone);
+      date.year = year;
+      date.month = month;
+      date.monthDay = day;
+      long millis;
+      millis = date.normalize(true);
+      
+      int flags = DateUtils.FORMAT_SHOW_DATE;
+      flags |= DateUtils.FORMAT_SHOW_YEAR;
+      String timeString = DateUtils.formatDateTime(getApplicationContext(), millis, flags);
+      
+      
+//      timeString += ", ";
+//      timeString += year;
+      mDateButton.setText(timeString);
+    }
+
+  }
+
+  public void showDatePickerDialog(View v) {
+    DialogFragment newFragment = new DatePickerFragment();
+    newFragment.show(getFragmentManager(), "datePicker");
+  }
 }
