db7fff525dd97150f5315c651a4b4eb87007353c
JaredPiedt <jared.piedt@gmail.com>
2014-07-15 22:21:30 -0400

    added news fragment and xml parser for rss feed

---
+++ GamecockMobile/src/com/gamecockmobile/news/NewsFragment.java
@@ -0,0 +1,16 @@
+package com.gamecockmobile.news;
+
+import android.app.ListFragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class NewsFragment extends ListFragment {
+  
+  public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    
+    return super.onCreateView(inflater, container, savedInstanceState);
+  }
+  
+}
